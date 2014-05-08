13cf44acad3c2282cedab901ffc517b2c17d63e5
David Guttman <david@davidguttman.com>
2014-05-08 12:10:36 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p config
-curl https://raw.github.com/davidguttman/node-cap-deploy/master/deploy.rb > config/deploy.rb
-curl https://raw.github.com/davidguttman/node-cap-deploy/master/opts.rb > config/opts.rb
+curl https://raw.githubusercontent.com/davidguttman/node-cap-deploy/master/deploy.rb > config/deploy.rb
+curl https://raw.githubusercontent.com/davidguttman/node-cap-deploy/master/opts.rb > config/opts.rb
 $EDITOR config/opts.rb
