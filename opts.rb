9832383e937798f87218f67bf5fd55ebfbe47431
David Guttman <david@davidguttman.com>
2014-10-15 11:45:20 -0600

    use node 10.32

--- opts.rb
+++ opts.rb
@@ -3,5 +3,4 @@ set :application_port, "port"
 role :app, "server.com"
 set :repository, "git@github.com:davidguttman/#{application}.git"
 
-set :node_version, '0.10.21'
-set :node_file, "index.js" # entry
+set :node_version, '0.10.32'
